a5edc7ac1d93295982a2f15d504e48ad9aefc86f
DevMaktubi <brenoluiz1@gmail.com>
2025-12-08 19:18:39 -0300

    Add functions for summing integers and their tails, along with corresponding tests

---
+++ iteration/iteration_test.go
@@ -0,0 +1,21 @@
+package iteration
+
+import (
+	"testing"
+	"fmt"
+)
+
+func TestRepeat(t *testing.T) {
+	repeated := Repeat("a", 5)
+	expected := "aaaaa"
+
+	if repeated != expected {
+		t.Errorf("Expected %q but got %q", expected, repeated)
+	}
+}
+
+func ExampleRepeat() {
+	repeated := Repeat("a",5)
+	fmt.Println(repeated)
+	// Output: aaaaa
+}
